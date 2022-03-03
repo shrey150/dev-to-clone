c5b3a588ce000388078989b81bb76dcaccb90cd4
Shrey Pandya <shrey150@yahoo.com>
2022-03-03 00:20:49 -0500

    Toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import toast from 'react-hot-toast'
 import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success("Hello toast!")}>
+        Toast Me
+      </button>
     </div>
   )
 }
